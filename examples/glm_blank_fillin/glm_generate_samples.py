f5eb4b307c6c8ce724234c181d60cd70651a1479
zhaohu xing <920232796@qq.com>
2022-05-25 14:32:52 +0800

    check blank fillin
    Signed-off-by: zhaohu xing <920232796@qq.com>

--- examples/glm_blank_fillin/glm_generate_samples.py
+++ examples/glm_blank_fillin/glm_generate_samples.py
@@ -15,8 +15,8 @@
 
 """Sample Generate GPT2"""
 import sys
-sys.path.append('/data/wang/models/GLMgeneration')
-sys.path.append('/data/wang/models/FlagAI')
+# sys.path.append('/data/wang/models/GLMgeneration')
+# sys.path.append('/data/wang/models/FlagAI')
 import os
 import torch
 
@@ -35,19 +35,29 @@ if __name__ == "__main__":
                                     fix_command_token=False)
 
     # Model,
-    model = GLMModel.from_pretrain(model_name='GLM-large-ch', download_path="./state_dict/")
+    # model = GLMModel.from_pretrain(model_name='GLM-large-ch', download_path="./state_dict/")
+    model = GLMModel.init_from_json("./state_dict/GLM-large-ch/config.json")
+    model.load_weights("./state_dict/GLM-large-ch/pytorch_model.bin")
     model.cuda(torch.cuda.current_device())
 
     predictor = Predictor(model, tokenizer)
     # generate samples
-    text = '问题：啤酒伤胃吗？回答：[gMASK]'
-    output=predictor.predict_generate_randomsample(text)
-    print(text,'\n',output)
+    text = ['问题：啤酒伤胃吗？回答：[gMASK]',
+            "问题：隔夜菜能吃吗？回答：[gMASK]",
+            "问题：如何评价许嵩？回答：[gMASK]"]
+    for t in text:
+        output=predictor.predict_generate_randomsample(t, top_k=50, repetition_penalty=4.0, top_p=1.0)
+        print(t,'\n',output)
 
-    text = '北京故宫是中国[MASK]非物质文化遗产。'
-    output = predictor.predict_generate_randomsample(text)
-    print(text, '\n', output)
+    text = ['北京故宫是中国[MASK]非物质文化遗产。',
+            "上海是中国[MASK]大都市。",
+            "天津大学是[MASK]现代大学。"]
+    for t in text:
+        output = predictor.predict_generate_randomsample(t, top_k=50, repetition_penalty=4.0, top_p=1.0)
+        print(t, '\n', output)
     #
-    text = '人工智能是一个以计算机科学为基础，由计算机、数学、哲学等多学科交叉融合的交叉学科，[sMASK]，具有非常巨大的前景。'
-    output = predictor.predict_generate_randomsample(text)
-    print(text, '\n', output)
+    text = ["人工智能是一个以计算机科学为基础，由计算机、数学、哲学等多学科交叉融合的交叉学科，[sMASK]，具有非常巨大的前景。",
+            "最近十多年来，人工神经网络的研究工作不断深入，已经取得了很大的进展，[sMASK]，表现出了良好的智能特性。"]
+    for t in text:
+        output = predictor.predict_generate_randomsample(t, top_k=50, repetition_penalty=4.0, top_p=1.0)
+        print(t, '\n', output)
