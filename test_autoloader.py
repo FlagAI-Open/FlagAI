545e873a5574e206d428fab5222c344b2f977591
zhaohu xing <920232796@qq.com>
2022-05-30 19:09:01 +0800

    add hint for autoloader
    Signed-off-by: zhaohu xing <920232796@qq.com>

--- test_autoloader.py
+++
@@ -1,8 +0,0 @@
-from flagai.auto_model.auto_loader import AutoLoader
-
-auto_loader = AutoLoader(
-    "seq2seq",
-    model_name="RoBERTa-bas",
-)
-model = auto_loader.get_model()
-tokenizer = auto_loader.get_tokenizer()
