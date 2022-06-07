16cb69f30b0368bc7ab8443377a2e54063e853ec
Anhforth <94831503+Anhforth@users.noreply.github.com>
2022-06-07 11:57:26 +0800

    enable automatic publish process (#48)
    * rebase
    
    * canceled uploading python package after push, now only release will trigger upload
    
    Signed-off-by: Anhforth <yanzhaodong2021@163.com>
    
    * removed uploading to testpypi; changed version to 1.0.1 in setup.py
    
    Signed-off-by: Anhforth <yanzhaodong2021@163.com>

--- setup.py
+++ setup.py
@@ -3,12 +3,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License")
 from setuptools import find_packages, setup
 
-with open("requirements.txt") as f:
-    required = f.read().splitlines()
-
 setup(
     name="flagai",
-    version="v1.0.0",
+    version="v1.0.1",
     description="FlagAI aims to help researchers and developers to freely train and test large-scale models for NLP tasks.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
@@ -17,7 +14,19 @@ setup(
     url="https://github.com/BAAI-Open/FlagAI",
     packages=find_packages(exclude="tests"),  # same as name
     license="Apache 2.0",
-    install_requires=required,
     include_package_data=True,
     python_requires=">=3.8",
+    install_requires=[
+        'nltk==3.6.7',
+        'sentencepiece==0.1.96',
+        'boto3==1.21.42',
+        'pandas==1.3.5',
+        'jieba==0.42.1',
+        'scikit-learn==1.0.2',
+        'tensorboard==2.9.0',
+        'transformers==4.18.0',
+        'datasets==2.0.0',
+        'setuptools==59.5.0',
+        'protobuf==3.20.1',
+    ]
 )
