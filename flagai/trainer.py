0271ba961b4678ff38cb717035ca6237df33ce23
fade-color <50774720+fade-color@users.noreply.github.com>
2022-07-14 10:40:58 +0800

    bug fix. (#68)
    * bug fix.
    
    * Update trainer.py
    
    remove print info
    
    Co-authored-by: Zac Liu <liuguang@baai.ac.cn>

--- flagai/trainer.py
+++ flagai/trainer.py
@@ -312,6 +312,7 @@ class Trainer():
                 rank = mpu.get_model_parallel_src_rank()
                 sampler = torch.utils.data.distributed.DistributedSampler(
                     dataset,
+                    # num_replicas=num_replicas,
                     rank=rank,
                     shuffle=shuffle)
             else:
@@ -471,6 +472,9 @@ class Trainer():
         for epoch in range(self.epochs):
             # log_dist('working on epoch {} ...'.format(epoch), [0])
             # Set the data loader epoch to shuffle the index iterator.
+            # if self.env_type == 'deepspeed+mpu':
+            #     if mpu.get_model_parallel_rank() == 0:
+            #         train_dataloader.sampler.set_epoch(epoch + self.world_size)
             if self.env_type != 'pytorch':
                 train_dataloader.sampler.set_epoch(epoch + self.world_size)
 
@@ -919,9 +923,9 @@ class Trainer():
                     labels = data_iterator['labels']
                 else:
                     labels = data_iterator['target_ids']
-
-                all_logits.append(logits)
-                all_labels.append(labels)
+                if len(self.metric_methods) != 0:
+                    all_logits.append(logits)
+                    all_labels.append(labels)
                 all_losses.append(lm_loss.view(1))
 
             if len(self.metric_methods) != 0:
