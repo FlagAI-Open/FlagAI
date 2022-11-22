159b0e5634ae23b286f9589f77a7131bf528071e
Zac Liu <liuguang@baai.ac.cn>
2022-11-22 18:33:33 +0800

    Revert "add_eva_clip"

--- flagai/data/tokenizer/clip/tokenizer.py
+++ flagai/data/tokenizer/clip/tokenizer.py
@@ -69,7 +69,7 @@ class ClipTokenizer(object):
     def __init__(self, bpe_path: str = default_bpe(), special_tokens=None):
         self.byte_encoder = bytes_to_unicode()
         self.byte_decoder = {v: k for k, v in self.byte_encoder.items()}
-        merges = gzip.open(bpe_path).read().decode("utf-8").split('\n')
+        merges = open(bpe_path).read().split('\n')
         merges = merges[1:49152-256-2+1]
         merges = [tuple(merge.split()) for merge in merges]
         vocab = list(bytes_to_unicode().values())
