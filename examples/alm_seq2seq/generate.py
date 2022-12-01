75902c1616fe59b04c8f9a0c0776c32e639d06f7
BAAI-OpenPlatform <open.platform@baai.ac.cn>
2022-12-01 14:52:10 +0800

    updated
    Signed-off-by: BAAI-OpenPlatform <open.platform@baai.ac.cn>

--- examples/alm_seq2seq/generate.py
+++ examples/alm_seq2seq/generate.py
@@ -4,12 +4,16 @@
 import torch
 from flagai.auto_model.auto_loader import AutoLoader
 from flagai.model.predictor.predictor import Predictor
+import random
+import numpy as np 
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-auto_loader = AutoLoader("seq2seq",
+auto_loader = AutoLoader("lm",
                          model_name="ALM-1.0",
                          model_dir="./checkpoints")
+
 model = auto_loader.get_model()
 tokenizer = auto_loader.get_tokenizer()
 
@@ -17,7 +21,7 @@ model.to(device)
 model.eval()
 predictor = Predictor(model, tokenizer)
 
-test_data = ['"مقالة - سلعة: يُعد الصدق هو الحل الأفضل']
+test_data = ["شرم الشيخ وجهة سياحية شهيرة [gMASK]"]
 for text in test_data:
     print('===============================================\n')
     print(text, ":")
@@ -26,14 +30,9 @@ for text in test_data:
         print('-----------random sample: --------------')
         print(
             predictor.predict_generate_randomsample(text,
-                                                    out_max_length=66,
+                                                    out_max_length=512,
                                                     top_k=10,
                                                     top_p=.1,
                                                     repetition_penalty=4.0,
                                                     temperature=1.2))
-        print('-----------beam search: --------------')
-        print(
-            predictor.predict_generate_beamsearch(text,
-                                                  out_max_length=66,
-                                                  beam_size=10))
         print()
