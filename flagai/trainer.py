64a794d956367bf9592766f4bc2aade2c25d4dac
ftgreat <ftgreat@github.com>
2023-02-03 10:49:21 +0800

    env_trainer trainer align comments
    Signed-off-by: ftgreat <ftgreat@github.com>

--- flagai/trainer.py
+++ flagai/trainer.py
@@ -330,6 +330,10 @@ class Trainer():
                                                shuffle=shuffle)
         else:
             if self.env_type == 'deepspeed+mpu':
+                # num_replicas = self.world_size // mpu.get_model_parallel_world_size(
+                # )
+                # rank = self.rank // mpu.get_model_parallel_world_size()
+                # rank = mpu.get_model_parallel_rank()
                 rank = mpu.get_model_parallel_src_rank()
                 print("*"*80)
                 print("local rank",self.rank, "model rank", rank)
