cd72c3e5f13a2aa23c94148d1ad366b7e8ebb837
ftgreat <ftgreat@163.com>
2023-04-18 14:16:07 +0800

    final datasets weights
    Signed-off-by: ftgreat <ftgreat@163.com>

--- flagai/data/dataset/indexed_dataset/build_weighted_datasets.py
+++ flagai/data/dataset/indexed_dataset/build_weighted_datasets.py
@@ -59,48 +59,48 @@ if __name__ == '__main__':
     batch1_tok100k = '/share/project/ldwang/data/indexed_dataset/batch1_tok100k'
     batch2_tok100k = '/share/project/ldwang/data/indexed_dataset/batch2_tok100k'
     data_prefix = [
-        1.5,
+        2.242990654,
         os.path.join(batch1_tok100k, 'en_dedup-md5-pile-openwebtext2_text_document'),
-        1.0,
+        5.046728972,
         os.path.join(batch1_tok100k, 'en_dedup-md5-pile-pile-cc_text_document'),
-        1.0,
+        13.64485981,
         os.path.join(batch1_tok100k, 'wudao-9_text_document'),
-        1.0,
+        2.336448598,
         os.path.join(batch1_tok100k, 'code_dedup-md5-pile-github_text_document'),
-        0.3,
+        1.869158879,
         os.path.join(batch1_tok100k, 'codegeex_text_document'),
-        5.0,
+        1.588785047,
         os.path.join(batch1_tok100k, 'en_dedup-md5-pile-wikipedia_en_text_document'),
-        1.2,
+        2.336448598,
         os.path.join(batch1_tok100k, 'cn_baike_text_document'),
-        1.5,
+        4.205607477,
         os.path.join(batch1_tok100k, 'pile-books_text_document'),
-        0.35,
+        0.186915888,
         os.path.join(batch1_tok100k, 'cn_ebook_merge_maxlen_text_document'),
-        1.2,
+        2.429906542,
         os.path.join(batch1_tok100k, 'pile-papers_text_document'),
-        2.0,
+        1.869158879,
         os.path.join(batch1_tok100k, 'en_dedup-md5-pile-stackexchange_text_document'),
-        1.0,
+        0.747663551,
         os.path.join(batch1_tok100k, 'cn_zhihu_text_document'),
 
-        0.8,
+        31.77570093,
         os.path.join(batch2_tok100k, 'ccnews_text_document'),
-        1.0,
+        12.42990654,
         os.path.join(batch2_tok100k, 'c4_text_document'),
-        0.8,
+        11.58878505,
         os.path.join(batch2_tok100k, 'wudao-3-8_text_document'),
-        5.0,
+        1.869158879,
         os.path.join(batch2_tok100k, 'hf-wiki_text_document'),
-        0.35,
+        0.654205607,
         os.path.join(batch2_tok100k, 'sjt_text_document'),
-        1.5,
+        1.214953271,
         os.path.join(batch2_tok100k, 'col_text_document'),
-        0.5,
+        1.121495327,
         os.path.join(batch2_tok100k, 'byg-cn_text_document'),
-        1.2,
+        0.093457944,
         os.path.join(batch2_tok100k, 'qa_text_document'),
-        1.2,
+        0.747663551,
         os.path.join(batch2_tok100k, 'wenge-zhihu-high_text_document'),
     ]
 
@@ -112,7 +112,9 @@ if __name__ == '__main__':
     ## 1000B = 1000 * 1000 * 1000 * 1000./ 2048 = 488281250
     train_max_num_samples = 195312500
     train_max_num_samples = 488281250
-    train_valid_test_num_samples = [train_max_num_samples, int(train_max_num_samples*0.0001)]
+    ## 1070B
+    train_max_num_samples = 522460937
+    train_valid_test_num_samples = [train_max_num_samples, int(train_max_num_samples*0.00001)]
     seq_length = 2048
     seed = 2023
     skip_warmup = True
@@ -125,10 +127,10 @@ if __name__ == '__main__':
     print("Total valid_dataset: ", len(valid_dataset))
 
     loader = torch.utils.data.DataLoader(
-        valid_dataset,
-        batch_size=1,
+        train_dataset,
+        batch_size=8,
         sampler=None,
-        num_workers=4,
+        num_workers=16,
         drop_last=False,
         pin_memory=False,
         prefetch_factor=4,
@@ -136,8 +138,7 @@ if __name__ == '__main__':
     
     ## a little testing
     for iteration_, batch in enumerate(loader, 0):
-        if iteration_%10000==0:
+        if iteration_%50000==0:
             print(f"step={iteration_}", flush=True)
-            break
     print("Ended data_loader testing.")
 
