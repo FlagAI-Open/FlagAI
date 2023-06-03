c0704a710acd438914c2290f8df163c8a6c24b19
ftgreat <ftgreat@163.com>
2023-06-03 08:14:42 +0000

    update tools/script/generate_valid_loss_sft.py
    Signed-off-by: ftgreat <ftgreat@163.com>

--- examples/gpt3_pretrain/llama/tools/script/generate_valid_loss_sft.py
+++ examples/gpt3_pretrain/llama/tools/script/generate_valid_loss_sft.py
@@ -11,22 +11,22 @@ from tqdm import tqdm
 
 from examples.gpt3_pretrain.build_index_mappings import _build_train_valid_test_datasets
 from examples.gpt3_pretrain.build_index_mappings import _build_train_valid_test_weighted_datasets
-from gpt import GPTLMHeadModel, combine_state_dicts_tp
+from examples.aquila.gpt import GPTLMHeadModel, combine_state_dicts_tp
 from flash_attn.models.llama import remap_state_dict_meta_llama, llama_config_to_gpt2_config
 from flash_attn.models.llama import config_from_checkpoint, state_dicts_from_checkpoint
 
-#checkpoints = '/data2/state_dict/'
-#model_name = 'Aquila-7b-67000'
-assert len(sys.argv) >= 4
-checkpoints = sys.argv[1]
-model_name = sys.argv[2]
-ckpt = sys.argv[3]
-model_list = [ckpt]
+assert len(sys.argv) >= 3
+model_checkpoints = sys.argv[1]
+model_iters = sys.argv[2]
+model_list = [os.path.join(model_checkpoints, model_iters)]
 
-cache_dir = os.path.join(checkpoints, model_name)
-tokenizer = Tokenizer.from_pretrained(model_name, cache_dir=cache_dir)
+## Just share same configs of tokenizer & model
+checkpoints = '/data2/ldwang/checkpoints'
+shared_model_name = 'Aquila-7b'
+cache_dir = os.path.join(checkpoints, shared_model_name)
+tokenizer = Tokenizer.from_pretrained(shared_model_name, cache_dir=cache_dir)
 
-config = llama_config_to_gpt2_config(config_from_checkpoint(checkpoints, model_name))
+config = llama_config_to_gpt2_config(config_from_checkpoint(checkpoints, shared_model_name))
 config.vocab_size=100008
 config.use_cache = False
 config.attn_pdrop = 0.0
@@ -41,6 +41,7 @@ config.use_flash_attn = True
 
 if True:
     max_seq_len = 2048
+    IGNORE_INDEX = -100
 
     import jsonlines
     import numpy as np
@@ -125,7 +126,7 @@ if True:
                     role_labels = copy.deepcopy(content)
                 else:
                     # masking
-                    role_labels = [env_args.IGNORE_INDEX] * len(content)
+                    role_labels = [IGNORE_INDEX] * len(content)
                 labels += role_labels
 
             example.append(EOS_TOKEN)
@@ -160,7 +161,7 @@ if True:
             labels = [data["labels"] for data in batch]
             max_length = max_seq_len
             input_ids = padding(input_ids, max_length)[:,:max_length]
-            labels = padding(labels, max_length, pad_idx=env_args.IGNORE_INDEX)[:,:max_length]
+            labels = padding(labels, max_length, pad_idx=IGNORE_INDEX)[:,:max_length]
     
             data = {
                 "input_ids": input_ids,
@@ -175,8 +176,18 @@ if True:
         valid_dataset = ConversationDatasetV2(conversations_val,
                                               tokenizer=tokenizer,
                                               maxlen=max_seq_len)
+    import torch
+    dataloader = torch.utils.data.DataLoader(
+        valid_dataset,
+        batch_size=1,
+        collate_fn=ConversationDatasetV2.collate_fn,
+        num_workers=4,
+        prefetch_factor=4,
+        pin_memory=True,
+        drop_last=False,
+        shuffle=False)
 
-model = GPTLMHeadModel(config, device='cuda:0', dtype=torch.float16)
+model = GPTLMHeadModel(config, device='cuda:1', dtype=torch.float16)
 for ck in model_list:
     checkpoint_path = os.path.join(f'{ck}', "pytorch_model.bin")
     ckpt_model = torch.load(checkpoint_path, map_location=torch.device('cpu'))
@@ -186,15 +197,13 @@ for ck in model_list:
     losses = []
     accuracy = []
     model.eval()
-    for d in tqdm(valid_dataset):
-        try:
-            output = model.forward(**d)
-        except Exception as e:
-            continue
+    for d in tqdm(dataloader):
+        batch = {x: d[x].to(next(model.parameters()).device)for x in d}
+        output = model.forward(**batch)
         losses += output.loss.view(-1).detach().cpu().numpy().tolist()
         accuracy += output.acc.view(-1).detach().cpu().numpy().tolist()
-        print(f"{ckpt} {sum(losses)/len(losses)})
+    print(f"{ck} {sum(losses)/len(losses)}")
 
-with open(f"{model_name}_sft_loss.log", 'a') as wf:
-    wf.write(f"{model_name} {ckpt} {sum(losses)/len(losses)} {sum(accuracy)/len(losses)}\n")
+    with open(f"{shared_model_name}_sft_loss.log", 'a') as wf:
+        wf.write(f"{shared_model_name} {ck} {sum(losses)/len(losses)} {sum(accuracy)/len(losses)}\n")
 
