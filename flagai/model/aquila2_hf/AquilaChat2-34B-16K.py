0178066b73505efbf293c9310d1e958b0d0abc1e
ldwang <ftgreat@gmail.com>
2023-10-18 15:46:20 +0800

    Add AquilaChat2-7B-16K.py AquilaChat2-7B-16K.py.
    Signed-off-by: ldwang <ftgreat@gmail.com>

---
+++ flagai/model/aquila2_hf/AquilaChat2-34B-16K.py
@@ -0,0 +1,14 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+device = torch.device("cuda:0")
+model_info = "BAAI/AquilaChat2-34B-16k"
+tokenizer = AutoTokenizer.from_pretrained(model_info, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(model_info, trust_remote_code=True, torch_dtype=torch.bfloat16)
+model.eval()
+model.to(device)
+text = "请给出10个要到北京旅游的理由。"
+from predict import predict
+out = predict(model, text, tokenizer=tokenizer, max_gen_len=200, top_p=0.95,
+              seed=1234, topk=100, temperature=0.9, sft=True, device=device,
+              model_name="AquilaChat2-34B-16K")
+print(out)
